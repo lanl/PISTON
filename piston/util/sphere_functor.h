fb0dcef78a0a9863826b4e30bcb53a0d3c48b2f7
ollie <lita.ollie@gmail.com>
2011-11-01 20:44:42 +0000

    added sphere function
    git-svn-id: svn://exponential.lanl.gov/usr/local/repository/piston@295 bcbcb40d-9a20-4f70-b08f-3d659b84208c

---
+++ piston/util/sphere_functor.h
@@ -0,0 +1,43 @@
+/*
+ * sphere.h
+ *
+ *  Created on: Sep 20, 2011
+ *      Author: ollie
+ */
+
+#ifndef SPHERE_H_
+#define SPHERE_H_
+
+#include <piston/implicit_function.h>
+
+namespace piston
+{
+
+template <typename IndexType, typename ValueType>
+struct sphere_functor : public piston::implicit_function3d<IndexType, ValueType>
+{
+    typedef piston::implicit_function3d<IndexType, ValueType> Parent;
+    typedef typename Parent::InputType InputType;
+
+    const IndexType x_o;
+    const IndexType y_o;
+    const IndexType z_o;
+    const IndexType radius;
+
+    sphere_functor(IndexType x, IndexType y, IndexType z, IndexType radius) :
+	x_o(x), y_o(y), z_o(z), radius(radius) {}
+
+    __host__ __device__
+    ValueType operator()(InputType pos) const {
+	const IndexType x = thrust::get<0>(pos);
+	const IndexType y = thrust::get<1>(pos);
+	const IndexType z = thrust::get<2>(pos);
+	const IndexType xx = x - x_o;
+	const IndexType yy = y - y_o;
+	const IndexType zz = z - z_o;
+	return (xx*xx + yy*yy + zz*zz);
+    }
+};
+
+} // namespace piston
+#endif /* SPHERE_H_ */
