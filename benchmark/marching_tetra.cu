b7a6a8152cd02894d3b0611bc706f2e82118289d
Li-Ta Lo <lita.ollie@gmail.com>
2012-10-31 11:06:48 -0600

    added benchmark for marching tetrahedron and its VTK counter part

--- benchmark/marching_tetra.cu
+++ benchmark/marching_tetra.cu
@@ -14,33 +14,37 @@
 #include "piston/image3d_to_tetrahedrons.h"
 #include "piston/marching_tetrahedron.h"
 
-
-//#define SPACE thrust::host_space_tag
-#define SPACE thrust::detail::default_device_space_tag
-
 using namespace piston;
 
 int
-main()
+main(int argc, char *argv[])
 {
+    int grid_size = 128;
+    if (argc > 1)
+	grid_size = atoi(argv[1])/2;
+
     vtkRTAnalyticSource *src = vtkRTAnalyticSource::New();
-    src->SetWholeExtent(-100, 100, -100, 100, -100, 100);
+    src->SetWholeExtent(-grid_size, grid_size, -grid_size, grid_size,
+                        -grid_size, grid_size);
     src->Update();
 
-    vtk_image3d<SPACE> image(src->GetOutput());
+    vtk_image3d<> image(src->GetOutput());
 
-    // get max and min of 3D scalars
-    float min_iso = *thrust::min_element(image.point_data_begin(), image.point_data_end());
-    float max_iso = *thrust::max_element(image.point_data_begin(), image.point_data_end());
+    // get max and min of scalars
+    float min_iso = *thrust::min_element(image.point_data_begin(),
+                                         image.point_data_end());
+    float max_iso = *thrust::max_element(image.point_data_begin(),
+                                         image.point_data_end());
 
-    typedef image3d_to_tetrahedrons<vtk_image3d<SPACE> > tetra_source;
+    typedef image3d_to_tetrahedrons<vtk_image3d<> > tetra_source;
     tetra_source tetra(image);
 
-    marching_tetrahedron<tetra_source, tetra_source> isosurface(tetra, tetra, 160.0f);
+    marching_tetrahedron<tetra_source> isosurface(tetra, tetra);
 
     struct timeval begin, end, diff;
     gettimeofday(&begin, 0);
-    for (float isovalue = min_iso; isovalue < max_iso; isovalue += ((max_iso-min_iso)/50)) {
+    for (float isovalue = min_iso; isovalue < max_iso;
+	 isovalue += ((max_iso-min_iso)/50.0f)) {
 	isosurface.set_isovalue(isovalue);
 	isosurface();
     }
@@ -48,7 +52,9 @@ main()
     timersub(&end, &begin, &diff);
 
     float seconds = diff.tv_sec + 1.0E-6*diff.tv_usec;
-    std::cout << "total time: " << seconds << ", fps: " << 50.f/seconds << std::endl;
+    std::cout << "grid_size: " << grid_size*2
+	      << ", total time: " << seconds
+	      << ", fps: " << 50.0f/seconds << std::endl;
     return 0;
 
 }
--- benchmark/vtkTetra.cpp
+++ benchmark/vtkTetra.cpp
@@ -7,20 +7,20 @@
 
 #include <vtkImageData.h>
 #include <vtkUnstructuredGrid.h>
-#include <vtkImageMandelbrotSource.h>
 #include <vtkRTAnalyticSource.h>
 #include <vtkDataSetTriangleFilter.h>
-#include <vtkDataSetToUnstructuredGridFilter.h>
 #include <vtkContourFilter.h>
 
 #include <algorithm>
 #include <sys/time.h>
 
 int
-main()
+main(int argc, char *argv[])
 {
+    int grid_size = atoi(argv[1])/2;
+
     vtkRTAnalyticSource *src = vtkRTAnalyticSource::New();
-    src->SetWholeExtent(-100, 100, -100, 100, -100, 100);
+    src->SetWholeExtent(-grid_size, grid_size, -grid_size, grid_size, -grid_size, grid_size);
     src->Update();
 
     vtkImageData *image = src->GetOutput();
@@ -31,29 +31,24 @@ main()
     float max_iso = *std::max_element((float *) image->GetScalarPointer(),
                                       (float *) image->GetScalarPointer() + image->GetNumberOfPoints());
 
-    std::cout << "min_iso: " << min_iso << std::endl;
-    std::cout << "max_iso: " << max_iso << std::endl;
-
     vtkDataSetTriangleFilter *tetra = vtkDataSetTriangleFilter::New();
     tetra->SetInput(src->GetOutput());
+    tetra->Update();
 
     vtkContourFilter *contour = vtkContourFilter::New();
     contour->SetInput(tetra->GetOutput());
-//    contour->Update();
     contour->GenerateValues(50, min_iso, max_iso);
 
     struct timeval begin, end, diff;
     gettimeofday(&begin, 0);
-//    for (float isovalue = min_iso; isovalue < max_iso; isovalue += ((max_iso-min_iso)/50)) {
-//	contour->SetValue(0, isovalue);
-//	contour->Update();
-//    }
+
     contour->Update();
+
     gettimeofday(&end, 0);
     timersub(&end, &begin, &diff);
 
     float seconds = diff.tv_sec + 1.0E-6*diff.tv_usec;
-    std::cout << "total time: " << seconds << ", fps: " << 50.f/seconds << std::endl;
+    std::cout << "grid_size: " << grid_size*2 << ", total time: " << seconds << ", fps: " << 50.0f/seconds << std::endl;
     return 0;
 }
 
