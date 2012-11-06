6ca40de21196c7eb5a3a4db8a5086ce04b8a3413
Chris Sewell <csewell@lanl.gov>
2012-11-06 10:12:53 -0700

    Addition to previous check-in

--- piston/image3d.h
+++ piston/image3d.h
@@ -29,6 +29,12 @@ ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSI
 #include <thrust/tuple.h>
 #include <thrust/iterator/counting_iterator.h>
 #include <thrust/iterator/transform_iterator.h>
+#include <piston/choose_container.h>
+
+#ifdef DISTRIBUTED_PISTON
+#include <piston/piston_math.h>
+#include <mpi.h>
+#endif
 
 namespace piston
 {
@@ -66,15 +72,99 @@ struct image3d
 	}
     };
 
+#ifdef DISTRIBUTED_PISTON
+    struct tuple2float3 : public thrust::unary_function<thrust::tuple<float, float, float>, float3>
+    {
+	__host__ __device__
+	float3 operator()(thrust::tuple<float, float, float> xyz) const {
+	    return make_float3((float) thrust::get<0>((xyz)),
+	                       (float) thrust::get<1>((xyz)),
+	                       (float) thrust::get<2>((xyz)));
+	}
+    };
+
+    thrust::device_vector<float> point_data_device;
+    thrust::device_vector<float3> grid_coord_device;
+    int NCells_local;
+#endif
+
     typedef typename thrust::counting_iterator<IndexType, MemorySpace> CountingIterator;
     typedef typename thrust::transform_iterator<grid_coordinates_functor, CountingIterator> GridCoordinatesIterator;
     GridCoordinatesIterator grid_coordinates_iterator;
+
+    typedef typename detail::choose_container<CountingIterator, float>::type PointDataContainer;
+    PointDataContainer point_data_vector;
+    typedef typename PointDataContainer::iterator PointDataIterator;
+
+    
 
     image3d(IndexType xdim, IndexType ydim, IndexType zdim) :
 	dim0(xdim), dim1(ydim), dim2(zdim),
 	NPoints(xdim*ydim*zdim),
 	NCells((xdim-1)*(ydim-1)*(zdim-1)),
 	grid_coordinates_iterator(CountingIterator(0), grid_coordinates_functor(xdim, ydim, zdim)) {}
+
+#ifdef DISTRIBUTED_PISTON
+    void distributeValues(bool includeGrid=true) {
+        int commSize;  (MPI_Comm_size(MPI_COMM_WORLD, &commSize));
+        int commRank;  (MPI_Comm_rank(MPI_COMM_WORLD, &commRank));
+
+        int pointsPerLayer = dim0*dim1;
+        int layers = dim2-1;
+        int layersPerRank = layers / commSize;
+
+        thrust::host_vector<float> point_data_local;       
+        thrust::host_vector<thrust::tuple<float, float, float> > grid_coord_tuples;
+        thrust::host_vector<float3> grid_coord_host, grid_coord_local; 
+        
+        int *sendCounts, *displs, *sendCounts3, *displs3;
+        int sendSize = pointsPerLayer*(layersPerRank+1);
+
+        if (commRank == 0)
+        { 
+          grid_coord_host.resize(NPoints); 
+          grid_coord_tuples.resize(NPoints); 
+          thrust::copy(grid_coordinates_begin(), grid_coordinates_end(), grid_coord_tuples.begin());
+          thrust::transform(grid_coord_tuples.begin(), grid_coord_tuples.end(), grid_coord_host.begin(), tuple2float3());
+
+          sendCounts = new int[commSize];
+          displs = new int[commSize];
+          for (unsigned int i=0; i<commSize; i++)
+          {
+            sendCounts[i] = sendSize;
+            displs[i] = i*pointsPerLayer*layersPerRank;
+          }
+
+          if (includeGrid)
+          {
+            sendCounts3 = new int[commSize];
+            displs3 = new int[commSize];
+            for (unsigned int i=0; i<commSize; i++)
+            {
+              sendCounts3[i] = 3*sendSize;
+              displs3[i] = 3*i*pointsPerLayer*layersPerRank;
+            }
+          }
+        }
+       
+        point_data_local.resize(sendSize);
+        (MPI_Scatterv(thrust::raw_pointer_cast(&*point_data_vector.begin()), sendCounts, displs, MPI_FLOAT, 
+                               thrust::raw_pointer_cast(&*point_data_local.begin()), sendSize, MPI_FLOAT, 0, MPI_COMM_WORLD));    
+        point_data_device = point_data_local;
+
+        if (includeGrid)
+        {
+          grid_coord_local.resize(sendSize);
+          (MPI_Scatterv(thrust::raw_pointer_cast(&*grid_coord_host.begin()), sendCounts3, displs3, MPI_FLOAT, 
+                                 thrust::raw_pointer_cast(&*grid_coord_local.begin()), 3*sendSize, MPI_FLOAT, 0, MPI_COMM_WORLD));    
+          grid_coord_device = grid_coord_local;
+        }
+
+        NCells_local = (dim0-1)*(dim1-1)*layersPerRank;
+
+        if (commRank == 0) { delete sendCounts; delete displs; if (includeGrid) { delete sendCounts3; delete displs3; } }
+    }
+#endif
 
     GridCoordinatesIterator grid_coordinates_begin() {
 	return grid_coordinates_iterator;
@@ -90,6 +180,13 @@ struct image3d
     }
     PhysicalCoordinatesIterator physical_coordinates_end() {
 	return grid_coordinates_iterator+this->NPoints;
+    }
+
+    PointDataIterator point_data_begin() {
+	return point_data_vector.begin();
+    }
+    PointDataIterator point_data_end() {
+	return point_data_vector.end();
     }
 };
 
